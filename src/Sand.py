b3cacf8fb1b8aa8fa85add56fcae5a982113ced3
Michael Dubno <michael@dubno.com>
2019-10-16 16:57:09 -0400

    Added new Nautilus drawing method.

--- src/Sand.py
+++ src/Sand.py
@@ -11,7 +11,7 @@ sandables = [
         "Rocks", "SpiralArms", "Sines",
         "Picture", "WebPic", "RandomDraw",
         "Bulbs", "Engine", "ESpiral",
-        "Grass",
+        "Grass", "Nautilus"
         ]
 
 todo = [ "Tree", "Celtic", "LOGO", "GreekKey", "CropCircle", "Propogation", ]
---
+++ src/draw/Nautilus.py
@@ -0,0 +1,70 @@
+from math import pow, radians, sin, cos, exp, pi 
+from Sand import *
+from dialog import *
+from Chains import *
+
+class Nautilus( Sandable ):
+    """
+### Nautilus spiral
+
+#### Parameters
+* **Inner radius** - Radius of the inside of the spiral.
+* **Turns** - Number of turns of the spiral.
+* **Sample rate** - Angular distance between points on the spiral.
+* **Curve factor** - Amount of curvature of the arc that makes up chambers. 
+* **Scale factor** - Speed of exponential growth.
+* **Logarithmic growth** - Percent of exponential growth per chamber.
+* **Fit to table** - map circular patterns to the shape of the table.
+* **X Center** and **Y Center** - where the center of the spiral will be relative to the table.
+"""
+
+    def __init__( self, width, length ):
+        self.editor = [
+            DialogFloat( "innerRadius",     "Inner radius",         units = "inches", min = 0.0, max = min(width,length) / 2. ),
+            DialogFloat( "turns",           "Turns",                default = 5., min = 1.0, max = 50.0 ),
+            DialogFloat( "angleRate",       "Sample rate",          units = "degrees", default = 11.0, min = 1, max = 45.0 ),
+            DialogFloat( "curveFactor",     "Curve factor",         default = 1., min = -6., max = 6. ),
+            DialogFloat( "scaleFactor",     "Scale factor",         default = 1., min = .01, max = 10.0 ),
+            DialogFloat( "logGrowth",       "Logarithmic growth",   units = "percent", default = 0., min = 0., max = 2. ),
+            DialogYesNo( "fitToTable",      "Fit to table"          ),
+            DialogBreak(),
+            DialogFloat( "xCenter",         "X Center",             units = "inches", default = width / 2.0 ),
+            DialogFloat( "yCenter",         "Y Center",             units = "inches", default = length / 2.0 ),
+        ]
+
+    def generate( self, params ):
+        xCenter = params.xCenter
+        yCenter = params.yCenter
+        
+        angleRate = params.angleRate
+        points = int( params.turns * 360.0 / angleRate)
+        curveAngle = radians(angleRate * params.curveFactor)
+        growth = .2 * params.scaleFactor
+        angleGrowth = 1.0 + params.logGrowth * .01
+
+        def pnt(angle, offset):
+            radius = exp((angle+offset)*growth) + params.innerRadius
+            x = xCenter + cos(angle) * radius
+            y = yCenter + sin(angle) * radius
+            return (x,y) 
+
+        chain = []
+        for point in range( points ):
+            pchain = []
+            angle   = radians( point * angleRate )
+            angleRate *= angleGrowth
+
+            pchain.append( pnt( angle, 0 ))
+            pchain.append( pnt( angle-curveAngle, -.5*pi ))
+            pchain.append( pnt( angle-curveAngle, -1.5*pi ))
+            pchain.append( pnt( angle-2.*pi, 0. ))
+            nchain = Chains.Spline( pchain )
+
+            chain += nchain
+            nchain.reverse()
+            chain += nchain
+
+        if params.fitToTable:
+            chain = Chains.circleToTable( chain, TABLE_WIDTH, TABLE_LENGTH )
+        return [chain]
+
